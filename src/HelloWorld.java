f939b89db7d8ff0622397764d2005b0d3168b94a
breezy123 <aufredericks@gmail.com>
2017-03-11 18:13:54 -0800

    prject initialised

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Created by aubrey on 11/03/2017.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args)
+    {
+        System.out.print("Hello World, welcome to intelliJ");
+    }
+}
